cfd61f49abec2cd48ed69c221a184bf8c68d5127
Murthy10 <samuel.kurath@gmail.com>
2018-01-31 14:04:49 +0100

    updated pgconfig

--- Postgres/db-settings.sh
+++ Postgres/db-settings.sh
@@ -6,34 +6,17 @@ set -o nounset
 function alter_system() {
 	echo "Altering System parameters"
 	PGUSER="$POSTGRES_USER" psql --dbname="$POSTGRES_DB" <<-EOSQL
-		alter system set autovacuum_work_mem = '4GB';
-		alter system set checkpoint_completion_target = '0.9';
-		alter system set checkpoint_timeout = '20min';
-		alter system set datestyle = 'iso, mdy';
-		alter system set default_statistics_target = '500';
-		alter system set default_text_search_config = 'pg_catalog.english';
-		alter system set dynamic_shared_memory_type = 'posix';
-		alter system set effective_cache_size = '96GB';
-		alter system set fsync = 'off';
-		alter system set lc_messages = 'en_US.utf8';
-		alter system set lc_monetary = 'en_US.utf8';
-		alter system set lc_numeric = 'en_US.utf8';
-		alter system set lc_time = 'en_US.utf8';
-		alter system set listen_addresses = '*';
-		alter system set log_checkpoints = 'on';
-		alter system set log_temp_files = '1MB';
-		alter system set log_timezone = 'UTC';
-		alter system set maintenance_work_mem = '96GB';
-		alter system set max_connections = '20';
-		alter system set random_page_cost = '1.1';
-		alter system set shared_buffers = '96GB';
-		alter system set synchronous_commit = 'off';
-		alter system set temp_buffers = '120MB';
-		alter system set timezone = 'UTC';
-		alter system set track_counts = 'on';
-		alter system set wal_buffers = '16MB';
-		alter system set max_wal_size = '5GB';
-		alter system set work_mem = '6GB';
+		ALTER SYSTEM SET max_connections = '1';
+		ALTER SYSTEM SET shared_buffers = '128GB';
+		ALTER SYSTEM SET effective_cache_size = '384GB';
+		ALTER SYSTEM SET work_mem = '64GB';
+		ALTER SYSTEM SET maintenance_work_mem = '2GB';
+		ALTER SYSTEM SET min_wal_size = '4GB';
+		ALTER SYSTEM SET max_wal_size = '8GB';
+		ALTER SYSTEM SET checkpoint_completion_target = '0.9';
+		ALTER SYSTEM SET wal_buffers = '16MB';
+		ALTER SYSTEM SET default_statistics_target = '500';
+		ALTER SYSTEM SET random_page_cost = '1.1';
 EOSQL
 }
 
